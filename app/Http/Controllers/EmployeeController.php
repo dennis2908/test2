caa388206ed5c91663243ba923a295c5a150c5cb
unknown <manullang_d@yahoo.com>
2021-05-28 15:27:20 +0700

    fix wrong controller

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -42,7 +42,7 @@ class EmployeeController extends Controller
                 })
 				->addColumn('company_view', function($row){
 						$companyViewBtn = '<a data-toggle="modal" data-target="#CompanyModal" href="javascript:void(0)" 
-						onclick="showModalCompany('.json_encode($row->id).')"
+						onclick="showModalCompany('.$row->company_id.')"
 						class="btn btn-info btn-sm"><svg width="14" height="14" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
 					  <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
 					  <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
